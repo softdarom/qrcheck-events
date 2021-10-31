bda43020102c36b6e5a55653d0d8505af0aa167b
Евгения Ковтун <Kovtun.E.E@sberbank.ru>
2021-10-31 18:15:52 +0300

    init

---
+++ src/test/java/ru/softdarom/qrcheck/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.softdarom.qrcheck.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
