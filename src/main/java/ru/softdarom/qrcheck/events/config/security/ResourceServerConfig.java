1d3aa98a66347a34fe92db9f6bb565703ea2a3f5
Евгения Ковтун <eekovtun@gmail.com>
2021-11-12 18:56:52 +0300

    QRC-81 Заменила oauth2 на использование внешней библиотеки

--- src/main/java/ru/softdarom/qrcheck/events/config/security/ResourceServerConfig.java
+++ src/main/java/ru/softdarom/qrcheck/events/config/security/ResourceServerConfig.java
@@ -1,34 +1,32 @@
 package ru.softdarom.qrcheck.events.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import ru.softdarom.qrcheck.events.config.property.ApiKeyProperties;
-import ru.softdarom.qrcheck.events.service.AuthHandlerExternalService;
 
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String DISABLED_RESOURCE_ID = null;
 
-    private final CustomAuthenticationProvider customAuthenticationProvider;
-    private final AuthenticationEntryPoint defaultAuthenticationEntryPoint;
-    private final AccessDeniedHandler defaultAccessDeniedHandler;
+    private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AccessDeniedHandler accessDeniedHandler;
 
     @Autowired
-    ResourceServerConfig(CustomAuthenticationProvider customAuthenticationProvider,
-                         AuthenticationEntryPoint defaultAuthenticationEntryPoint,
-                         AccessDeniedHandler defaultAccessDeniedHandler) {
-        this.customAuthenticationProvider = customAuthenticationProvider;
-        this.defaultAuthenticationEntryPoint = defaultAuthenticationEntryPoint;
-        this.defaultAccessDeniedHandler = defaultAccessDeniedHandler;
+    ResourceServerConfig(@Qualifier("qrCheckAuthenticationProvider") AuthenticationProvider authenticationProvider,
+                         @Qualifier("qrCheckAuthenticationEntryPoint") AuthenticationEntryPoint authenticationEntryPoint,
+                         @Qualifier("qrCheckAccessDeniedHandler") AccessDeniedHandler accessDeniedHandler) {
+        this.authenticationProvider = authenticationProvider;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.accessDeniedHandler = accessDeniedHandler;
     }
 
     @Override
@@ -48,11 +46,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                                 .antMatchers("/inner/**").hasRole("API_KEY")
                                 .antMatchers("/mobile/**").hasAnyRole("USER")
                 )
-                .authenticationProvider(customAuthenticationProvider)
+                .authenticationProvider(authenticationProvider)
                 .exceptionHandling(handlerConfigurer ->
                         handlerConfigurer
-                            .authenticationEntryPoint(defaultAuthenticationEntryPoint)
-                            .accessDeniedHandler(defaultAccessDeniedHandler)
+                            .authenticationEntryPoint(authenticationEntryPoint)
+                            .accessDeniedHandler(accessDeniedHandler)
                 )
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -64,14 +62,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(ResourceServerSecurityConfigurer resources) {
         resources
                 .resourceId(DISABLED_RESOURCE_ID)
-                .authenticationEntryPoint(defaultAuthenticationEntryPoint)
-                .accessDeniedHandler(defaultAccessDeniedHandler);
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler);
 
     }
-
-    @Bean
-    ResourceServerTokenServices tokenServices(ApiKeyProperties properties, AuthHandlerExternalService authHandlerExternalService) {
-        return new RemoteOAuth2TokenService(properties.getToken().getOutgoing(), authHandlerExternalService);
-    }
-
 }
