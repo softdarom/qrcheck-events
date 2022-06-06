519bd2f5c1d899713284b5c98102c73113d058d8
Евгения Ковтун <eekovtun@gmail.com>
2022-06-06 18:54:08 +0300

    QRC-145

--- src/main/java/ru/softdarom/qrcheck/events/config/swagger/annotations/ApiGetAllEventTypes.java
+++ src/main/java/ru/softdarom/qrcheck/events/config/swagger/annotations/ApiGetAllEventTypes.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import ru.softdarom.qrcheck.events.model.dto.LocaleTypeDto;
@@ -25,8 +26,11 @@ import static java.lang.annotation.ElementType.METHOD;
                 @Parameter(
                         in = ParameterIn.HEADER,
                         name = "Accept-Language",
-                        description = "Header для установки locale",
-                        example = "ru",
+                        description = "Заголовок для установки языка локализации",
+                        examples = {
+                                @ExampleObject(name = "Russian", value = "ru", description = "Русский язык"),
+                                @ExampleObject(name = "English", value = "en", description = "Английский язык")
+                        },
                         schema = @Schema(implementation = String.class)
                 )
         },
